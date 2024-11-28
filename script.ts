c338e0ad5deea12633db79e7f67b3da921850daa
devroy10 <roychinwuba@gmail.com>
2024-11-28 01:41:46 +0100

    initial commit

---
+++ script.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+// use `prisma` in your application to read and write data in your DB
+
+// putting the main function into an async function is because almost everything in prisma runs async
+async function main() {
+    try {
+        const users = await prisma.user.findMany()
+        console.log(users);
+    } catch (error) {
+        console.error('Error creating user:', error);
+    }
+}
+
+main()
+
+    .catch(e => {
+        console.error(e.message)
+    })
+    .finally(async () => {
+        await prisma.$disconnect
+    })
